2c67fbe6aa6b031e57084b763d2cd96d96a5dba2
Brian Schulte <brianr.schulte@gmail.com>
2020-05-08 23:41:19 -0400

    update menu

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,12 @@
 export default {
+  menu: [
+    "Introduction",
+    "Getting Started",
+    "Lists",
+    "Fields",
+    "Lifecycle Hooks",
+    "Access Control",
+  ],
   dest: "/dist",
   base: "/zephyr-cloud-docs/dist/",
 };
